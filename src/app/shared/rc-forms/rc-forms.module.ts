18a6c3290a3100071b377a5c393a87d426bae4f3
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-11-18 13:24:05 +0100

    feat: buttons

--- src/app/shared/rc-forms/rc-forms.module.ts
+++ src/app/shared/rc-forms/rc-forms.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './components/input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SelectComponent } from './components/select/select.component';
+import { ButtonComponent } from './components/button/button.component';
 
 
 
 @NgModule({
-  declarations: [InputComponent, SelectComponent],
+  declarations: [InputComponent, SelectComponent, ButtonComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  exports: [InputComponent, SelectComponent],
+  exports: [InputComponent, SelectComponent, ButtonComponent],
 })
 export class RcFormsModule { }
