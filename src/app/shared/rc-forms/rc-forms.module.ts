e474fa59836922a050655fc005db49f26fe1ed3f
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-11-19 10:00:42 +0100

    refactor: clean out all px

--- src/app/shared/rc-forms/rc-forms.module.ts
+++ src/app/shared/rc-forms/rc-forms.module.ts
@@ -5,15 +5,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SelectComponent } from './components/select/select.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ChipsComponent } from './components/chips/chips.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
 
 
 
 @NgModule({
-  declarations: [InputComponent, SelectComponent, ButtonComponent, ChipsComponent],
+  declarations: [InputComponent, SelectComponent, ButtonComponent, ChipsComponent, CheckboxComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  exports: [InputComponent, SelectComponent, ButtonComponent, ChipsComponent],
+  exports: [InputComponent, SelectComponent, ButtonComponent, ChipsComponent, CheckboxComponent],
 })
 export class RcFormsModule { }
