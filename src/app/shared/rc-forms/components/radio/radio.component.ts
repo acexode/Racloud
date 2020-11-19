53ca2ccdf74bb3d9a1b080a207a3e2810a8a3123
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-11-19 18:46:18 +0100

    feat: radio component completed

--- src/app/shared/rc-forms/components/radio/radio.component.ts
+++ src/app/shared/rc-forms/components/radio/radio.component.ts
@@ -1,18 +1,75 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 @Component({
-  selector: 'app-radio',
+  selector: 'app-radios',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.scss']
+  styleUrls: ['./radio.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: RadioComponent,
+      multi: true,
+    },
+  ],
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
   @Input() checked: boolean;
-  constructor() { }
+  formGroup = this.fb.group({
+    radio: this.fb.control(null),
+  });
+
+  onChange: (_: any) => void;
+  onTouched: () => void;
+  value: any;
+
+  constructor(private fb: FormBuilder, private cdRef: ChangeDetectorRef) { }
+  writeValue(obj: any): void {
+    this.value = obj;
+    this.formGroup.setValue({ radio: obj });
+    this.formGroup.updateValueAndValidity();
+    this.cdRef.markForCheck();
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    if(isDisabled) {
+      this.formGroup.disable({ emitEvent: true });
+    } else {
+      this.formGroup.enable({ emitEvent: true });
+    }
+    this.cdRef.markForCheck();
+  }
 
   ngOnInit(): void {
+    this.formGroup.valueChanges.subscribe((vals) => {
+      if (this.onChange) {
+        this.onChange(this.getFieldValue());
+      }
+    });
   }
   get isChecked() {
     return this.checked ?? false;
   }
+
+  getFieldValue() {
+    const field = this.formGroup.get('radio');
+    return field ? field.value : null;
+  }
+
 
 }
