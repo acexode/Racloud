0991eb5b357ba4be7c60c20815c258a0b2e915eb
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-11-24 15:49:14 +0100

    feat: completed tab as style guide

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements OnInit {
-
+  @Input() selectedTemplate: TemplateRef<any>;
   constructor() { }
 
   ngOnInit(): void {
