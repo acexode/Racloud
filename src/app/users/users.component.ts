f5892e93fd4fa48756e4357bc00287602d09551f
Acexode <abdulwahab.abubakar@softescu.com>
2020-12-02 16:27:35 +0100

    rc-9

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -156,7 +156,7 @@ filterTable(filterObj: TableFilterConfig) {
 setDropUp(row) {
   const idx = this.rowData.findIndex(e => e.id == row.id) + 1;
   console.log(idx)
-  const mod = idx % 10 == 0 ? 10 : idx % 10;
+  const mod = idx % 10 === 0 ? 10 : idx % 10;
   console.log(mod)
   if(mod < 6) {
     this.isDropup = false;
