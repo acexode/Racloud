b33faac7a00313f25b214241c15f47ed3719069e
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-12-04 14:15:04 +0100

    feat: working on authentication

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: '',
+  serverUrl: 'https://ra-core.azure.softescu.com',
   fakeBackend: true,
   recaptchaKey: '6LdFYvwUAAAAAJVVUGjb70IOCuccNKf2w4E9wVcx',
 };
