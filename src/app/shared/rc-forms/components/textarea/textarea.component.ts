11c9bbf8c36a4e7f02b0de2d58b723ee65ce901e
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-12-07 10:32:03 +0100

    feat: textarea

--- src/app/shared/rc-forms/components/textarea/textarea.component.ts
+++ src/app/shared/rc-forms/components/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-textarea',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./textarea.component.scss']
 })
 export class TextareaComponent implements OnInit {
-
+  @Input() config;
   constructor() { }
 
   ngOnInit(): void {
