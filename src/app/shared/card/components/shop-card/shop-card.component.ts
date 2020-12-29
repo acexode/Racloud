7ba5622a3191a11198168b5b9f83173b7fdd4f58
ereyomi oluwaseyi <ereyomi.oluwaseyi@softescu.com>
2020-12-29 16:44:09 +0100

    feat: types

--- src/app/shared/card/components/shop-card/shop-card.component.ts
+++ src/app/shared/card/components/shop-card/shop-card.component.ts
@@ -7,6 +7,56 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ShopCardComponent implements OnInit {
   @Input() item: any;
+  type = {
+    wl: {
+      initTitle: 'WL',
+      productName: 'RA Workshop',
+      productVersion: 'Lite',
+      bgColor: 'rc-accent-blue-bg',
+    },
+    we: {
+      initTitle: 'WE',
+      productName: 'RA Workshop',
+      productVersion: 'Express',
+      bgColor: 'rc-accent-blue-bg',
+    },
+    wp: {
+      initTitle: 'WP',
+      productName: 'RA Workshop',
+      productVersion: 'Professional',
+      bgColor: 'rc-accent-blue-bg',
+    },
+    cnc: {
+      initTitle: 'CNC',
+      productName: 'RA Workshop',
+      productVersion: 'CNC Add-on',
+      bgColor: 'rc-accent-blue-bg',
+    },
+    wcs: {
+      initTitle: 'WCS',
+      productName: 'RA Workshop',
+      productVersion: 'Client Server',
+      bgColor: 'rc-accent-blue-bg',
+    },
+    shp: {
+      initTitle: 'WCS',
+      productName: 'RA Workshop',
+      productVersion: '12 Support hours pack',
+      bgColor: 'rc-accent-blue-160-bg',
+    },
+    pn: {
+      initTitle: 'PN',
+      productName: '',
+      productVersion: '',
+      bgColor: 'rc-grey-bg',
+    },
+    etc: {
+      initTitle: 'ETC',
+      productName: '',
+      productVersion: '',
+      bgColor: 'rc-black-bg',
+    },
+  };
   constructor() { }
 
   ngOnInit(): void {
