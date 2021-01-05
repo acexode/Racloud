43d0ad82fed967d1af751c7a6b5bbe3883256ab5
Acexode <abdulwahab.abubakar@softescu.com>
2021-01-05 06:50:37 +0100

    rc-28

--- src/app/shared/rc-forms/models/card-item-model.ts
+++ src/app/shared/rc-forms/models/card-item-model.ts
@@ -1,18 +1,18 @@
 export interface CardItem {
     Id?: string | number;
     type?: string;
-    ProductId: number,
-    PriceListId: number,
+    ProductId: number;
+    PriceListId: number;
     productName?: string;
     productVersion?: string;
-    Discount: number,
-    DiscountPrc: number,
-    SupportHours: number,
+    Discount: number;
+    DiscountPrc: number;
+    SupportHours: number;
     Value?: string | number;
     RenewalValue?: string | number;
     Product: {
-        Id: number,  
-        Name: string,  
-        Description:string  
-    }  
+        Id: number;
+        Name: string;
+        Description:string;
+    }
 };
