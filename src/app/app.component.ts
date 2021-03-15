14b0579a2358af285e7b2ee499f4a434420e47da
Acexode <abdulwahab.abubakar@softescu.com>
2021-03-15 23:20:18 +0100

    rc-88

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,7 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
         }
       }
     );
-    
+
     this.titleService.handleNavigationTitle().subscribe((v) => {
       if (v && v === 'Not found') {
 
