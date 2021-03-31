3959c37c4774a6542cc0163520c8f7276463db58
Adrian Pintilie <adrian.pintilie@softescu.com>
2021-03-31 08:55:41 +0000

    Update environment.test.ts

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'https://ra-core.azure.softescu.com',
+  serverUrl: 'https://ra-core-test.azure.softescu.com',
   fakeBackend: true,
   recaptchaKey: '6LdFYvwUAAAAAJVVUGjb70IOCuccNKf2w4E9wVcx',
   appVersion: require('./../../package.json').version,
